6deedcf04fb725e558f9353d84bf3c4d23ea8b5c
Samuel Goodwin <samuel.goodwin@me.com>
2012-12-04 14:55:19 +0100

    Initial Commit

---
+++ FacebookTest/RWSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RWSAppDelegate.h
+//  FacebookTest
+//
+//  Created by Samuel Goodwin on 12/4/12.
+//  Copyright (c) 2012 Roundwall Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacebookTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FacebookTest
+//
+//  Created by Samuel Goodwin on 12/4/12.
+//  Copyright (c) 2012 Roundwall Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RWSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RWSAppDelegate class]));
+    }
+}
